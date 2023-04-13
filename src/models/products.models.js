6886fbd4a684930dadd7bdfa9fd32d451fe224c2
matsguevara <mateomarketing100@gmail.com>
2023-04-12 23:31:08 -0500

    firts commit

--- src/models/products.models.js
+++ src/models/products.models.js
@@ -1,4 +1,47 @@
-const Products = 'hola :D'
+const { DataTypes } = require('sequelize')
+
+const db = require('../utils/database')
+
+
+const Products = db.define('products', {
+    
+        id : {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+            len: [1, 255]
+        }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+              isNumeric: true
+            }
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+            validate: {
+            isNumeric: true
+        }
+    }
+})
+
+
 
 module.exports = Products
 
--- src/products/products.router.js
+++ src/products/products.router.js
@@ -1,5 +1,16 @@
 const router = require('express').Router()
 
+const productServices = require("./products.services");
 
+
+router.route("/")
+  .get(productServices.getAllProducts)
+  .post(productServices.postProduct);
+
+
+router.route("/:id")
+  .get(productServices.getProductById)
+  .patch(productServices.patchProduct)
+  .delete(productServices.deleteProduct);
 
 module.exports = router
